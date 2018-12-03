0a65770a419d769d9961ff7a478a8f7270a0f3b4
ryanhull <ryhu23@gmail.com>
2018-12-03 10:00:27 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_heroku import Heroku
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+@app.route('/')
+def home():
+        return "<h1>Welcome</h1>"
+
+
+if __name__ == "__main__": 
+    app.debug = True
+    app.run()
